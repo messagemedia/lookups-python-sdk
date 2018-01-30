71fcf4229877a20df5a9850d859817841c895306
James Dale <git@jamesdale.com.au>
2018-01-30 14:35:23 +1100

    Update useragent

--- message_media_lookups/controllers/base_controller.py
+++ message_media_lookups/controllers/base_controller.py
@@ -32,7 +32,7 @@ class BaseController(object):
     http_call_back = None
 
     global_headers = {
-        'user-agent': 'messagemedia-lookups'
+        'user-agent': 'messagemedia-lookups-python-sdk-1.0.0'
     }
 
     def __init__(self, client=None, call_back=None):
